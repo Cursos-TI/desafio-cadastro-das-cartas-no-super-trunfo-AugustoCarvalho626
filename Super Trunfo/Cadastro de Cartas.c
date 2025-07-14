fd159adbdc86c0572d2397115d66301e1e9723de
AugustoCarvalho626 <augustojipa2008@hotmail.com>
2025-07-14 18:28:46 +0000

    atualizando os codigo

--- Super Trunfo/Cadastro de Cartas.c
+++ Super Trunfo/Cadastro de Cartas.c
@@ -27,8 +27,42 @@
 // Inserção de dados da carta 1
         printf("Digite os dados da carta 1, de acordo com os itens solicitados: \n");
 
-        printf("Digite a letra para o estado da carta 1:\n");
-       // scanf("%c", &estado1);
+        printf("Digite uma letra para o estado da carta 1: ");
+        scanf("%c", &estado1);
+        
+        printf("Digite o código da carta 1, contendo uma letra e dois números: ");
+        scanf("%s", &codigo_carta1);
+
+        printf("Digite o nome da cidade: ");
+        scanf("%s", &nome_cidade1);
+
+        printf("Digite o número da população: ");
+        scanf("%i", &população1);
+
+        printf("Digite a área em Km²: ");
+        scanf("%.2f", &area1);
+
+        printf("Digite o PIB: ");
+        scanf("%i", &pib1);
+
+// Inserção dos dados da carta 2
+        
+        printf("Digite os dados da carta 2, de acordo com os itens solicitados: \n");
+
+        printf("Digite uma letra para o estado da carta 2: ");
+        scanf("%c", &estado2);
+
+        printf("Digite o código da carta 2, contendo uma letra e dois números: ");
+        scanf("%S", &codigo_carta2);
+
+        printf("Digite o nome da cidade: ");
+        scanf("%S", &nome_cidade2);
+
+        printf("Digite o número da população: ");
+
+
+
+
         return 0;
 
     }
